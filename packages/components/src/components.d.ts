cc7e03cbeaf27a18d6dee509e41ff77b7133627b
Ronan <crnoogle@gmail.com>
2024-10-28 18:02:37 +0800

    feat: 新增components.d.ts

---
+++ packages/components/src/components.d.ts
@@ -0,0 +1,34 @@
+import * as components from "../index";
+declare module "@vue/runtime-core" {
+  export interface GlobalComponents {
+    RDropdownButton: typeof components.RDropdownButton;
+    RPopselectButton: typeof components.RPopselectButton;
+    RPopconfirmButton: typeof components.RPopconfirmButton;
+    RDnd: typeof components.RDnd;
+    RHoverContainer: typeof components.RHoverContainer;
+    RSimpleLayout: typeof components.RSimpleLayout;
+    RScroll: typeof components.RScroll;
+    RSelect: typeof components.RSelect;
+    RTab: typeof components.RTab;
+    RTabItem: typeof components.RTabItem;
+    RTabList: typeof components.RTabList;
+    RTabSelector: typeof components.RTabSelector;
+    RTabItemContextMenu: typeof components.RTabItemContextMenu;
+    RTableColumnSetting: typeof components.RTableColumnSetting;
+    RTableColumnSettingItem: typeof components.RTableColumnSettingItem;
+    RTableHeaderOperations: typeof components.RTableHeaderOperations;
+    RTableOrderQuery: typeof components.RTableOrderQuery;
+    RTableOrderQueryItem: typeof components.RTableOrderQueryItem;
+    RTableOrderQueryNewer: typeof components.RTableOrderQueryNewer;
+    RTableWhereQuery: typeof components.RTableWhereQuery;
+    RTableWhereQueryAsyncSelect: typeof components.RTableWhereQueryAsyncSelect;
+    RTableWhereQueryDatePicker: typeof components.RTableWhereQueryDatePicker;
+    RTableWhereQueryDateRangePicker: typeof components.RTableWhereQueryDateRangePicker;
+    RTableWhereQueryInput: typeof components.RTableWhereQueryInput;
+    RTableWhereQueryItem: typeof components.RTableWhereQueryItem;
+    RTableWhereQuerySelect: typeof components.RTableWhereQuerySelect;
+    RTableLabelValueRender: typeof components.RTableLabelValueRender;
+    RTable: typeof components.RTable;
+  }
+}
+export {};
--- packages/components/src/components/table/index.ts
+++ packages/components/src/components/table/index.ts
@@ -1,6 +1,11 @@
+import { withInstall } from '~/src/composables'
+import Table from './index.vue'
+
 export * from './column-setting'
 export * from './header-operations'
-export { default as RTable } from './index.vue'
 export * from './query'
 export * from './row-expand'
 export * from './types'
+
+export const RTable = withInstall(Table)
+export default RTable
